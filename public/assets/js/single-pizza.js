f618e4176222bc15310577ca73310ed8b6f230a8
cpesar <77510555+cpesar@users.noreply.github.com>
2021-07-15 21:17:44 -0600

    added indexeddb for offline functionality

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -158,6 +158,29 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  // Handle PUT requests for replies
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if(!response.ok){
+      throw new Error('Something went wrong');
+    }
+    response.json();
+  })
+  .then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err);
+  }); 
 }
 
 $backBtn.addEventListener('click', function() {
